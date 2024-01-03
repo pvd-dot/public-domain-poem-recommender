3453a54acc4cdeeacf8290996354c3ebe17cbfa3
patrickvando <patrickvando@gmail.com>
2024-01-03 12:07:05 -0800

    Update CLI output

--- src/recommender/main.py
+++ src/recommender/main.py
@@ -13,10 +13,17 @@ def main():
     chat = chatgpt.ChatGPT()
     recs = recommender.Recommender(vectorsearcher, chat)
 
+    print(
+        "Welcome to the poem recommender.\n You can ask the poem recommender"
+        + "anything, and it will try to recommend you a relevant poem.\n Enter"
+        + "'quit' to exit.\n\n"
+    )
     while True:
         query_text = input("User: ")
+        if query_text == "quit":
+            break
         explanation, poem_text = recs.ask(query_text)
-        print(f"Recommender: {explanation}\n\n{poem_text}")
+        print(f"Recommender: {explanation}\n\n{poem_text}\n")
 
 
 if __name__ == "__main__":
